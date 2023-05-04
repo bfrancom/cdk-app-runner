9bcc5d54b4d6353c98e6d92969ba6d3b421b58ad
Ben Francom <5726729+bfrancom@users.noreply.github.com>
2023-05-04 09:48:45 -0600

    all the things

--- lib/cdk-app-runner-stack.ts
+++ lib/cdk-app-runner-stack.ts
@@ -1,3 +1,5 @@
+import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
+
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -7,7 +9,17 @@ export class CdkAppRunnerStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
+    const service = new apprunner.Service(this, 'Service', {
+      source: apprunner.Source.fromEcrPublic({
+        imageConfiguration: { port: 8000 },
+        imageIdentifier: 'public.ecr.aws/aws-containers/hello-app-runner:latest',
+      }),
+    });
+    new cdk.CfnOutput(this, 'apprunner-url', {
+      value: service.serviceUrl,
+      description: 'App Runner URL',
+      exportName: 'apprunner-url',
+    });
     // example resource
     // const queue = new sqs.Queue(this, 'CdkAppRunnerQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
